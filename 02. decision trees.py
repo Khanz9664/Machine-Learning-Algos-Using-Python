f957e4dd123923605b0d69533e4271a6cfe0393e
Shahid Ul Islam <65996596+Khanz9664@users.noreply.github.com>
2024-11-08 20:13:05 +0530

    Update 02. decision trees.py

--- 02. decision trees.py
+++ 02. decision trees.py
@@ -3,13 +3,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
-''here we import the iris dataset using datasets avaliable in sklearn library and train the decision tree model on that dataset ''
+'''here we import the iris dataset using datasets avaliable in sklearn library and train the decision tree model on that dataset '''
 
 iris = load_iris()
 X = iris.data
 Y = iris.target
 
-''Here we are splitting the datasets using train_test_split 70% of data is used for training process and 50% of data is used for testing the model ''
+'''Here we are splitting the datasets using train_test_split 70% of data is used for training process and 50% of data is used for testing the model '''
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
 classfier = DecisionTreeClassifier()
